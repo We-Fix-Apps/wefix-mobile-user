6d5e116740cbb32765e5a2516f796a7623d1bfc1
Jadaboawwad <jadaboawwad@gmail.com>
2025-12-20 16:42:57 +0300

    fix(mobile): remove duplicate code in home_screen, auth_helper, and dropdown_card_item
    - Remove duplicate code blocks outside of classes
    - Fix compilation errors in home_screen.dart
    - Fix duplicate method definitions in auth_helper.dart
    - Fix duplicate class definition in dropdown_card_item.dart

--- lib/Presentation/B2B/ticket/components/dropdown_card_item.dart
+++ lib/Presentation/B2B/ticket/components/dropdown_card_item.dart
@@ -27,32 +27,3 @@ class DropdownCardItem {
   int get hashCode => id.hashCode;
 }
 
-
-
-  final int id;
-  final String title;
-  final String? subtitle;
-  final IconData? icon;
-  final Widget? iconWidget;
-  final Map<String, dynamic>? data;
-
-  DropdownCardItem({
-    required this.id,
-    required this.title,
-    this.subtitle,
-    this.icon,
-    this.iconWidget,
-    this.data,
-  });
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-    return other is DropdownCardItem && other.id == id;
-  }
-
-  @override
-  int get hashCode => id.hashCode;
-}
-
-
