820b47cbeea4cf3306ed8705a0dd98de54839263
Jadaboawwad <jadaboawwad@gmail.com>
2026-01-31 12:58:57 +0300

    deploy : release after merge

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         // Ensure it's at least 23 for firebase_auth
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 39
+        versionCode = 40
         versionName = flutter.versionName
     }
 
