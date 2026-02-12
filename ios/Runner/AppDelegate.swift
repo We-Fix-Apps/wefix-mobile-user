a93e4f63f6a781d420acd3b2368f1132351a1fcd
Jadaboawwad <jadaboawwad@gmail.com>
2026-02-12 12:46:36 +0300

    feat : ios notification added

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,5 @@
-// import FirebaseCore
-// import FirebaseMessaging
+import FirebaseCore
+import FirebaseMessaging
 import Flutter
 import GoogleMaps
 import UIKit
@@ -15,36 +15,54 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     // تهيئة Firebase عند بدء التطبيق
-    // FirebaseApp.configure()
+    FirebaseApp.configure()
+    
     GMSServices.provideAPIKey("AIzaSyB_gK5Q70PoYdlP08CH8NfTyWsePdvS9e0")
     GeneratedPluginRegistrant.register(with: self)
+    
+    // طلب إذن الإشعارات
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self
+      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+      UNUserNotificationCenter.current().requestAuthorization(
+        options: authOptions,
+        completionHandler: { _, _ in }
+      )
+    } else {
+      let settings: UIUserNotificationSettings =
+        UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+      application.registerUserNotificationSettings(settings)
+    }
+    
+    application.registerForRemoteNotifications()
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
   // =========================================
   // تسجيل الجهاز لتلقي إشعارات الدفع (Push Notifications)
   // =========================================
-  // override func application(
-  //   _ application: UIApplication,
-  //   didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-  // ) {
-  //   // ربط APNs مع Firebase Messaging لإرسال واستقبال الإشعارات
-  //   Messaging.messaging().apnsToken = deviceToken
-  //   // استدعاء التنفيذ الافتراضي للأب
-  //   super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-  // }
+  override func application(
+    _ application: UIApplication,
+    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+  ) {
+    // ربط APNs مع Firebase Messaging لإرسال واستقبال الإشعارات
+    Messaging.messaging().apnsToken = deviceToken
+    // استدعاء التنفيذ الافتراضي للأب
+    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+  }
 
   // =========================================
   // استقبال الإشعارات أثناء الخلفية أو عند الإغلاق
   // =========================================
-  // override func application(
-  //   _ application: UIApplication,
-  //   didReceiveRemoteNotification userInfo: [AnyHashable: Any],
-  //   fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
-  // ) {
-  //   super.application(
-  //     application, didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler
-  //   )
-  // }
+  override func application(
+    _ application: UIApplication,
+    didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+    fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+  ) {
+    super.application(
+      application, didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler
+    )
+  }
 
 }
